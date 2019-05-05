9b22aa4175a164840f420e663a70a0a03f6a948e
Anton Popov <ademaro@ya.ru>
2019-05-05 15:15:19 +0800

    Добавлена конфигурация для таск-раннера

---
+++ gulpfile.babel.js
@@ -0,0 +1,102 @@
+import {src, dest, watch, parallel, series} from 'gulp';
+import del from 'del';
+import sass from 'gulp-sass';
+import csso from 'gulp-csso';
+import pug from 'gulp-pug';
+import rename from 'gulp-rename';
+// import babel from 'gulp-babel';
+import browserSync from 'browser-sync';
+import autoprefixer from 'gulp-autoprefixer';
+import fs from 'fs';
+import data from 'gulp-data';
+import imagemin from 'gulp-imagemin';
+import webp from 'imagemin-webp';
+import jpegoprim from 'imagemin-jpegoptim';
+
+/**
+ *  Основные директории
+ */
+const dirs = {
+  src: 'src',
+  dest: 'build'
+};
+
+/**
+ * Пути к файлам
+ */
+const path = {
+  styles: {
+    watch: `${dirs.src}/sass/**/*.scss`,
+    compile: `${dirs.src}/sass/style.scss`,
+    save: `${dirs.dest}/css`
+  },
+  views: {
+    data: `${dirs.src}/pug/data/data.json`,
+    watch: `${dirs.src}/pug/**/*.pug`,
+    compile: `${dirs.src}/pug/pages/*.pug`,
+    save: `${dirs.dest}`
+  },
+  scripts: {
+    watch: `${dirs.src}/js/**/*.js`,
+    save: `${dirs.dest}/js`
+  }
+};
+
+/**
+ * Основные задачи
+ */
+export const styles = () => src(path.styles.compile)
+  .pipe(sass.sync().on('error', sass.logError))
+  .pipe(dest(path.styles.save))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(csso())
+  .pipe(rename({
+    suffix: `.min`
+  }))
+  .pipe(dest(path.styles.save));
+
+export const views = () => src(path.views.compile)
+  .pipe(data((file) => {
+    return JSON.parse(
+      fs.readFileSync(path.views.data)
+    );
+  }))
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(pug())
+  .pipe(dest(path.views.save));
+
+export const scripts = () => src(path.scripts.watch)
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(dest(path.scripts.save));
+
+export const clean = () => del([dirs.dest]);
+
+export const devWatch = () => {
+  const bs = browserSync.init({
+    server: dirs.dest,
+    notify: false
+  });
+  watch(path.styles.watch, styles).on('change', bs.reload);
+  watch(path.views.watch, views).on('change', bs.reload);
+  // watch(sources.scripts, scripts);
+};
+
+/**
+ * Задачи для разработки
+ */
+// export const dev = series(clean, parallel(buildStyles, buildViews, buildScripts), devWatch);
+export const dev = series(clean, parallel(styles, views), devWatch);
+
+/**
+ * Для билда
+ */
+// export const build = series(clean, parallel(styles, views, scripts, images));
+
+export default dev;
